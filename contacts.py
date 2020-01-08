341ceb97be3729669e6119d877730c69560d34f1
jeSoosRemirez <dkhomych0091@gmail.com>
2020-01-08 13:49:24 +0200

    ITS WORKING!

--- contacts.py
+++ contacts.py
@@ -1,7 +1,12 @@
 import json
 
 
-number = {'Andrew': 38063}
+def contactadd(self):
+    name = str(input('Type name of your friend: '))
+    phonenum = int(input('Type phone number: '))
+    contacts = {name: phonenum}
+    with open('../proj/contacts.txt', 'w') as file:
+        file.write(json.dumps(contacts))
 
-with open('../proj/contacts.txt', 'w') as file:
-    file.write(json.dumps(number))
+
+contactadd(self=1)
